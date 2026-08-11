9a7a1fa533595061fe05c889b60255d9d517441e
mrtysk <257210068+mrtysk@users.noreply.github.com>
2026-08-11 10:35:01 +0900

    React入門#２課題提出

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import { useMemo } from "react";
+import "./counterApp.css";
+import "./caluculateApp.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  const [priceText, setPriceText] = useState("");
+  const [quantityText, setQuantityText] = useState("");
+
+  const taxExcluded = useMemo(() => {
+    const result = Number(priceText) * Number(quantityText);
+    return result.toFixed(2);
+  }, [priceText, quantityText]);
+
+  const taxIncluded = useMemo(() => {
+    const result = taxExcluded * 1.1;
+    return result.toFixed(2);
+  }, [taxExcluded]);
+
+  const clear = () => {
+    setPriceText("");
+    setQuantityText("");
+  };
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      <div className="counterContainer">
+        <p className="p-big">カウンターアプリ</p>
+
+        <button onClick={increment}>ここを押す</button>
+        <button onClick={reset}>リセット</button>
+
+        <p>現在の数字: {count}</p>
+        {count % 10 === 0 && count > 0 && (
+          <p id="achievement">{count} 回クリック達成！</p>
+        )}
+      </div>
+
+      <div className="calculateContainer">
+        <p className="p-big">合計専用計算機</p>
+        <p>価格: {priceText} 円</p>
+        <input
+          type="number"
+          value={priceText}
+          onChange={(event) => setPriceText(event.target.value)}
+          placeholder="価格を入力"
+          min="1"
+        />
+
+        <p>数量: {quantityText} 個</p>
+        <input
+          type="number"
+          value={quantityText}
+          onChange={(event) => setQuantityText(event.target.value)}
+          placeholder="数量を入力"
+          min="1"
+        />
+
+        <button onClick={clear}>クリア</button>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          {priceText !== "" && quantityText !== "" && (
+            <>
+              <p id="taxExcluded">税抜:{taxExcluded}円</p>
+              <p id="taxIncluded">税込:{taxIncluded}円</p>
+            </>
+          )}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
