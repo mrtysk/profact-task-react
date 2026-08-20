d4402b565842597195d341c279ce27c14b52b256
mrtysk <257210068+mrtysk@users.noreply.github.com>
2026-08-20 09:18:48 +0900

    React入門#3 課題提出 2回目

--- src/App.tsx
+++ src/App.tsx
@@ -28,13 +28,13 @@ function App() {
 }
 
 function CommentList() {
-  const [comments, setComments] = useState([
+  const comments = [
     { id: 1, author: "Alice", text: "素晴らしい記事です！" },
     { id: 2, author: "Bob", text: "面白かったです！！！" },
     { id: 3, author: "Noah", text: "楽しく読ませていただきました。" },
     { id: 4, author: "Alice", text: "ためになる！" },
     { id: 5, author: "Bob", text: "興味深い。" },
-  ]);
+  ];
 
   return (
     <div>
