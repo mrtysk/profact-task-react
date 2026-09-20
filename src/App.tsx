00a27700cd257046dfd6e527b42ce3a3b903e9e7
mrtysk <257210068+mrtysk@users.noreply.github.com>
2026-09-20 17:39:21 +0900

    React＃2 課題提出

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// シンプルなカウンターアプリを作ろう
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+function Counter() {
+  const [count, setCount] = useState(0);
 
-      <div className="ticks"></div>
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  if (count % 10 === 0 && count !== 0) {
+    return (
+      <div className="appContainer">
+        <p className="appTitle">カウンターアプリ</p>
+        <button onClick={handleClick}>クリック</button>
+        <p>現在のカウント：{count}</p>
+        <p className="strongPoint">{count}回クリック達成！</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="appContainer">
+        <p className="appTitle">カウンターアプリ</p>
+        <button onClick={handleClick}>クリック</button>
+        <p>現在のカウント：{count}</p>
+      </div>
+    );
+  }
 }
 
-export default App
+// 商品価格の計算機を作ろう
+
+function Calculator() {
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  //   const handlePrice = (event) => {
+  //     setPrice(event.target.value);
+  //   };
+
+  //   const handleQuantity = (event) => {
+  //     setQuantity(event.target.value);
+  //   };
+
+  const withoutTax = () => {
+    return Number(price) * Number(quantity);
+  };
+
+  const addTax = () => {
+    return withoutTax() * 0.1;
+  };
+  const withTax = () => {
+    return withoutTax() + addTax();
+  };
+
+  if (1 <= Number(price) && 1 <= Number(quantity)) {
+    return (
+      <div className="appContainer">
+        <p className="appTitle">商品価格の計算機</p>
+        <div>
+          <p>値段:</p>
+          <input
+            type="number"
+            value={price}
+            // onChange={handlePrice}
+            onChange={(event) => setPrice(event.target.value)}
+            placeholder="価格を入力"
+          />
+        </div>
+        <div>
+          <p>数量:</p>
+          <input
+            type="number"
+            value={quantity}
+            // onChange={handleQuantity}
+            onChange={(event) => setQuantity(event.target.value)}
+            placeholder="数量を入力"
+          />
+        </div>
+        <div>
+          <p>{withoutTax()}円</p>
+          <p className="strongPoint">税込:{withTax()}円</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="appContainer">
+        <p className="appTitle">商品価格の計算機</p>
+        <div>
+          <p>値段:</p>
+          <input
+            type="number"
+            value={price}
+            // onChange={handlePrice}
+            onChange={(event) => setPrice(event.target.value)}
+            placeholder="価格を入力"
+          />
+        </div>
+        <div>
+          <p>数量:</p>
+          <input
+            type="number"
+            value={quantity}
+            // onChange={handleQuantity}
+            onChange={(event) => setQuantity(event.target.value)}
+            placeholder="数量を入力"
+          />
+        </div>
+        <div>
+          <p>円</p>
+          <p>税込:円</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Counter />
+      </div>
+      <div>
+        <Calculator />
+      </div>
+    </>
+  );
+}
